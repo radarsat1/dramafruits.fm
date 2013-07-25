55d12d3fe5e706e9a249ee2b50082293481576db
Stephen Sinclair <sinclair@music.mcgill.ca>
2013-07-25 18:36:44 +0200

    Fix run.

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+BRANCH="$(basename $(cat .git/HEAD | cut -d' ' -f2))"
 python get.py >out.html
 git checkout gh-pages
 mv out.html index.html
 git add index.html
 git commit -m "$(date)"
-git checkout master
+git checkout "$BRANCH"
