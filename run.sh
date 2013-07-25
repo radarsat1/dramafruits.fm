c2b6771661894d2a457bf0498fc3afb91625d71a
Stephen Sinclair <sinclair@music.mcgill.ca>
2013-07-25 18:39:50 +0200

    Add style.css to run.sh.

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 BRANCH="$(basename $(cat .git/HEAD | cut -d' ' -f2))"
 python get.py >out.html
+cp style.css __tmp.css
 git checkout gh-pages
 mv out.html index.html
-git add index.html
+mv __tmp.css style.css
+git add index.html style.css
 git commit -m "$(date)"
 git checkout "$BRANCH"
